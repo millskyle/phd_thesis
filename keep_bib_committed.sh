b0f88271a3920176a067c0a6fed5c70e7a3be240
Kyle Mills <kyle.mills@uoit.net>
2021-01-14 13:15:16 -0500

    updated with mac locations

--- keep_bib_committed.sh
+++ keep_bib_committed.sh
@@ -1,10 +1,12 @@
 
 
 
-local_loc='/home/kmills/bibtex/KMPhD.bib'
+local_loc='/Users/kmills/Documents/bibtex/KMPhD.bib'
 
 cp $local_loc ./
 git add $(basename $local_loc)
 git commit -m "automatic bib commit"
 git push
+
+date > last_committed.log
 
