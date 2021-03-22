8e2252f96e1fb63380bb8bd638076edc95635ad8
Kyle Mills <kyle.mills@uoit.net>
2021-03-22 13:25:27 -0400

    updated;

--- keep_bib_committed.sh
+++ keep_bib_committed.sh
@@ -1,10 +1,13 @@
 
 
 
+cd /home/kmills/git/phd_thesis/
+
 local_loc='/home/kmills/bibtex/KMPhD.bib'
 
-cp $local_loc ./
-git add $(basename $local_loc)
-git commit -m "automatic bib commit"
-git push
-
+if ! $(diff $local_loc ./); then
+	cp $local_loc ./
+	git add $(basename $local_loc) \
+	&& git commit -m "automatic bib commit" \
+	&& git push
+fi
